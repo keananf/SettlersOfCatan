3f39e854aea7c38a1e28cb3eeabf7629a6c77846
keananf <keananf@live.com>
2016-11-17 12:15:34 +0000

    Building classes

---
+++ core/src/game/build/City.java
@@ -0,0 +1,25 @@
+package game.build;
+
+import game.enums.ResourceType;
+
+import java.util.*;
+
+/**
+ * Class describing a city
+ * @author 140001596
+ */
+public class City extends Building
+{
+	/**
+	 * @return a map containing the total cost for all resources
+	 */
+	public Map<ResourceType, Integer> getCost()
+	{
+		Map<ResourceType, Integer> resources = new HashMap<ResourceType, Integer>();
+		
+		resources.put(ResourceType.Wheat, 2);
+		resources.put(ResourceType.Stone, 3);
+		
+		return resources;
+	}
+}

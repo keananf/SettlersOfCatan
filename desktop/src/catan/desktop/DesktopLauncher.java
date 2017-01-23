e42b0965c977a7d6219c2ffb3cbc3df61faf43f3
Liam McMenemie <lm251@st-andrews.ac.uk>
2017-01-23 18:38:25 +0000

    Set minimum window dimensions

--- desktop/src/catan/desktop/DesktopLauncher.java
+++ desktop/src/catan/desktop/DesktopLauncher.java
@@ -1,15 +1,19 @@
 package catan.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import catan.SettlersOfCatan;
 
 public class DesktopLauncher {
+	private static final int INITIAL_WINDOW_WIDTH = 1024;
+	private static final int INITIAL_WINDOW_HEIGHT = 576;
+
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
-		config.height = 576;
-		config.title = "Settlers of Catan";
-		new LwjglApplication(new SettlersOfCatan(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Settlers of Catan");
+		config.setWindowedMode(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT);
+		config.setWindowSizeLimits(3*INITIAL_WINDOW_WIDTH/4, INITIAL_WINDOW_HEIGHT, -1, -1);
+
+		new Lwjgl3Application(new SettlersOfCatan(), config);
 	}
 }

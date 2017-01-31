0dff4bc6112ff2219a3a72c36c1dc3fb1a6cd8df
cak8 <cak8@st-andrews.ac.uk>
2017-01-31 11:17:04 +0000

    progress on sendMove

--- core/src/main/java/catan/ClientWorker.java
+++ core/src/main/java/catan/ClientWorker.java
@@ -1,13 +1,19 @@
+package catan;
+
 import board.Edge;
 import board.Node;
 import client.ClientGame;
+import enums.Colour;
 import game.InProgressTurn;
-import enums.Move;
-import enums.ClickObject;
+import protocol.BoardProtos.NodeProto;
+import protocol.BuildProtos.*;
+import protocol.BuildProtos.RoadProto;
+import protocol.RequestProtos.Request;
 
 public class ClientWorker
 {
 	private ClientGame game;
+	private InProgressTurn inProgressTurn = game.inProgressTurn;
 
 	public ClientWorker(ClientGame game)
 	{
@@ -16,21 +22,20 @@ public class ClientWorker
 
 	public void update()
 	{
-		InProgressTurn inProgressTurn = ClientGame.inProgressTurn;
 		
 		if(inProgressTurn.chosenMove != null)
 		{
 			sendMove();
 		}
-		else if(inProgressTurn.initialClickObject == ClickObject.CARD)
+		else if(inProgressTurn.initialClickObject == inProgressTurn.initialClickObject.CARD)
 		{
 			checkCard();
 		}
-		else if(inProgressTurn.initialClickObject == ClickObject.NODE)
+		else if(inProgressTurn.initialClickObject == inProgressTurn.initialClickObject.NODE)
 		{
 			checkBuild(inProgressTurn.chosenNode);
 		}
-		else if(inProgressTurn.initialClickObject == ClickObject.EDGE)
+		else if(inProgressTurn.initialClickObject == inProgressTurn.initialClickObject.EDGE)
 		{
 			checkBuild(inProgressTurn.chosenEdge);
 		}
@@ -54,7 +59,48 @@ public class ClientWorker
 
 	private void sendMove()
 	{
+		Request.Builder request = Request.newBuilder();
+
+		switch(game.inProgressTurn.chosenMove){
+			case BUILD_ROAD:
+				request.setBuildRoadRequest(setUpRoad(inProgressTurn.chosenEdge,col));
+				break;
+            case BUILD_SETTLEMENT:
+				request.setBuildSettlementRequest(setUpSettlement(x1 ,y1, type, col));//type settlement
+				break;
+            case UPGRADE_SETTLEMENT:
+                request.setupradeSettlementRequest();// becomes city
+                break;
+            case BUY_CARD:
+                request.buyDevCardRequest();
+                break;
+            case CLOSE:
+                request.endMoveRequest();
+                break;
+		}
+
 		//TODO: create protocol buffer from game state
 		//TODO: send protocol buffer to server
 	}
+
+	private RoadProto setUpRoad(Edge edge, Colour col)
+    {
+        RoadProto.Builder road = RoadProto.newBuilder();
+		PointProto.Builder node = PointProto.newBuilder();
+
+
+        //Node1
+        node.setX(edge.getX().getX());
+        node.setY(edge.getX().getY());
+        road.setP1(node.build());
+
+        //Node2
+        node.setX(x2);
+        node.setY(y2);
+        road.setP2(node.build());
+
+        road.setPlayerId(col);
+
+        return road.build();
+    }
 }

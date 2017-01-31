1527dc6ceb118680a1a6b96ad2f76e15a9ed89d4
Liam McMenemie <liam@lmcm.io>
2017-01-31 23:34:00 +0000

    Space ship!

--- core/src/main/java/catan/ui/GameScreen.java
+++ core/src/main/java/catan/ui/GameScreen.java
@@ -3,41 +3,81 @@ package catan.ui;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import catan.SettlersOfCatan;
 
 public class GameScreen implements Screen
 {
+	final private PerspectiveCamera cam;
+	final private CameraInputController camController;
+	final private AssetManager assets;
+	final private ModelBatch modelBatch;
+	final private Array<ModelInstance> instances = new Array<ModelInstance>();
+	final private Environment environment;
+
 	final private SettlersOfCatan game;
-	final private Stage ui = new Stage(new ScreenViewport());
 
 	public GameScreen(final SettlersOfCatan game)
 	{
 		this.game = game;
-		Gdx.input.setInputProcessor(ui);
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(0f, 7f, 10f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
+
+		assets = new AssetManager();
+		assets.load("ship.g3db", Model.class);
+		assets.finishLoading();
+
+		modelBatch = new ModelBatch();
+		instances.add(new ModelInstance(assets.get("ship.g3db", Model.class)));
+
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 	}
 
 	@Override
 	public void render(final float delta)
 	{
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		ui.act(delta);
-		ui.draw();
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instances, environment);
+		modelBatch.end();
 	}
 
 	@Override
 	public void resize(final int width, final int height)
 	{
-		ui.getViewport().update(width, height, true);
 	}
 
 	@Override
 	public void dispose()
 	{
-		ui.dispose();
+		modelBatch.dispose();
+		instances.clear();
+		assets.dispose();
 	}
 
 	// Required but unused

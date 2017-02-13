b40324373dbaf3cce7abfaa689ebc28559b5485c
Liam McMenemie <liam@lmcm.io>
2017-02-13 18:33:46 +0000

    feat(camera): Add zoom control with UP and DOWN

--- core/src/main/java/catan/ui/SpinCamController.java
+++ core/src/main/java/catan/ui/SpinCamController.java
@@ -10,9 +10,10 @@ public final class SpinCamController implements InputProcessor
 	private static final Vector3 ORIGIN = new Vector3(0f, 0f, 0f);
 	private static final Vector3 Y_AXIS = new Vector3(0f, 1f, 0f);
 	private static final float SPIN_RATE = 4f;
+	private static final float ZOOM_RATE = 0.9f;
 
 	private final Camera camera;
-	private boolean leftHeld, rightHeld;
+	private boolean leftHeld, rightHeld, upHeld, downHeld;
 
 	public SpinCamController(final Camera camera)
 	{
@@ -24,6 +25,10 @@ public final class SpinCamController implements InputProcessor
 			camera.rotateAround(ORIGIN, Y_AXIS, -SPIN_RATE);
 		if (rightHeld)
 			camera.rotateAround(ORIGIN, Y_AXIS, SPIN_RATE);
+		if (upHeld)
+			camera.position.scl(ZOOM_RATE);
+		if (downHeld)
+			camera.position.scl(1/ZOOM_RATE);
 
 		camera.update();
 	}
@@ -35,6 +40,10 @@ public final class SpinCamController implements InputProcessor
 			leftHeld = true;
 		if (keycode == Input.Keys.RIGHT)
 			rightHeld = true;
+		if (keycode == Input.Keys.UP)
+			upHeld = true;
+		if (keycode == Input.Keys.DOWN)
+			downHeld = true;
 
 		return true;
 	}
@@ -46,6 +55,10 @@ public final class SpinCamController implements InputProcessor
 			leftHeld = false;
 		if (keycode == Input.Keys.RIGHT)
 			rightHeld = false;
+		if (keycode == Input.Keys.UP)
+			upHeld = false;
+		if (keycode == Input.Keys.DOWN)
+			downHeld = false;
 
 		return true;
 	}

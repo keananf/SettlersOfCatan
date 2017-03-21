02f75b47ccd9fc9fbf96d6c7a117f053556b7ca4
Beth <bg32@st-andrews.ac.uk>
2017-03-21 14:01:14 +0000

    Added a method to check if a user has clicked on an edge.
    takes coordinate makes and equation of a line checkls if point lies on that line. Does this for all edges

--- core/src/main/java/catan/ui/GameController.java
+++ core/src/main/java/catan/ui/GameController.java
@@ -71,16 +71,102 @@ class GameController implements InputProcessor
 			double heighestHeight = hex.centre.z + ((Math.sqrt(3)*WIDTH)/4);
 			double lowestHeight = hex.centre.z - ((Math.sqrt(3)*WIDTH)/4);
 
+			
+			
+			
+			
+			
+			
+			
+			
 			if (planeX <= furtherstRight && planeX >= furthestLeft)
 			{
 				if (planeY <= heighestHeight && planeY >= lowestHeight)
-				{
+				{ 
 					return hex;
 				}
 			}
 		}
 		return null;
 	}
+	
+	
+	
+	public boolean isEdge(float planeX, float planeY){
+		for(GameObject hex : screen.hexes){
+		final float WIDTH = 2f;
+		double furthestLeft = hex.centre.x - WIDTH/2;
+		double furthestRight = hex.centre.x + WIDTH/2;
+		double heighestHeight = hex.centre.z + ((Math.sqrt(3)*WIDTH)/4);
+		double lowestHeight = hex.centre.z - ((Math.sqrt(3)*WIDTH)/4);
+		double inRight = hex.centre.x + WIDTH/4;
+		double inLeft = hex.centre.x - WIDTH/4;
+		double cons1;
+		double cons2;
+		
+		
+		
+		
+		double gradient1 = (inRight-furthestRight)/(heighestHeight-hex.centre.y);
+		double gradient2 = (-1)/gradient1;
+		if(heighestHeight>0){
+		cons1 = (gradient1*(inRight*=-1))+(heighestHeight*=-1);
+		
+		} else{
+			cons1 = (gradient1*(inRight*=-1))-(heighestHeight*=-1);
+
+		} 
+		if(lowestHeight>0){
+			cons2 = (gradient1*(inLeft*=-1))+(lowestHeight*=-1);
+		} else{
+			cons2 = (gradient1*(inLeft*=-1))-(lowestHeight*=-1);
+
+		}
+		
+		if((gradient1*planeX)+cons1 == planeY ||(gradient1*planeX)+cons2 == (planeY) ){
+			return true;
+			
+			
+			
+		
+		} 
+		 if(heighestHeight>0){
+			 cons1 = (gradient2*(inLeft*=-1))+(heighestHeight*=-1);
+		 } else{
+			 cons1 = (gradient2*(inLeft*=-1))-(heighestHeight*=-1);
+
+		 } if(lowestHeight>0){
+				cons2 = (gradient2*(inLeft*=-1))+(lowestHeight*=-1);
+				
+		 } else{
+				cons2 = (gradient2*(inLeft*=-1))-(lowestHeight*=-1);
+
+		 }
+		
+		
+		 if((gradient2*planeX)+cons1 == planeY ||(gradient2*planeX)+cons2 == (planeY) ){
+				return true;
+				
+				
+				
+			
+			} 
+		
+		}
+		
+		return false;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	@Override public boolean keyUp(int keycode) { return false; }
 	@Override public boolean keyDown(int keycode) { return false; }

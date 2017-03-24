2f724275a19af2d569bbcdd7da7e47b8630c977b
Beth <bg32@st-andrews.ac.uk>
2017-03-24 19:11:27 +0000

    Changing MoveBuilder to send a turn given the current state of the board

--- core/src/main/java/catan/ui/MoveBuilder.java
+++ core/src/main/java/catan/ui/MoveBuilder.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import grid.Node;
 import intergroup.Requests;
 import client.Client;
+import game.build.Building;
+import game.build.Settlement;
 import grid.BoardElement;
 import grid.Edge;
 import grid.Hex;
@@ -18,28 +20,37 @@ private final Client client;
 		this.client = client;	
 	}
 	
-	protected void onSelect(BoardElement element)
-	{
-		try {
-		    Semaphore lock = client.getTurnLock();
-		    lock.acquire();
-		
-		    if(element instanceof Node){
-		    	client.getTurn().setChosenNode((Node) element);
-		    	client.getTurn().setChosenMove(Requests.Request.BodyCase.BUILDSETTLEMENT);
-		    } if(element instanceof Edge){
-		    	client.getTurn().setChosenEdge((Edge) element);
-		    	client.getTurn().setChosenMove(Requests.Request.BodyCase.BUILDROAD);           	
-		    	
-		    }if(element instanceof Hex){
-		    	client.getTurn().setChosenHex((Hex) element);
-		    	client.getTurn().setChosenMove(Requests.Request.BodyCase.MOVEROBBER);
-		    }
-		
-		    client.sendTurn();
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		} //TODO   
-    }
+	protected void onSelect(BoardElement element) {
 
+		if (element instanceof Node) {
+			client.getTurn().setChosenNode((Node) element);
+			Building building = ((Node) element).getSettlement();
+			if (building == null) {
+				client.getTurn().setChosenMove(Requests.Request.BodyCase.BUILDSETTLEMENT);
+				
+			} else if (building instanceof Settlement) {
+				client.getTurn().setChosenMove(Requests.Request.BodyCase.BUILDCITY);
+				
+			} else {
+				return;
+			}
+
+		}
+		if (element instanceof Edge) {
+			client.getTurn().setChosenEdge((Edge) element);
+			if (((Edge) element).getRoad() == null) {
+				client.getTurn().setChosenMove(Requests.Request.BodyCase.BUILDROAD);
+
+			}
+		} else {
+			return;
+		}
+
+		if (element instanceof Hex) {
+			client.getTurn().setChosenHex((Hex) element);
+			client.getTurn().setChosenMove(Requests.Request.BodyCase.MOVEROBBER);
+		}
+		
+		client.acquireLocksAndSendTurn(client.getTurn());
+	}
 }

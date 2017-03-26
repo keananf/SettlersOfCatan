53dc069d178063b5713f399a159516df2353eed4
Beth <bg32@st-andrews.ac.uk>
2017-03-26 19:04:32 +0100

    Adding a method to draw a road and to draw a building

--- core/src/main/java/catan/ui/GameScreen.java
+++ core/src/main/java/catan/ui/GameScreen.java
@@ -2,6 +2,8 @@ package catan.ui;
 
 import catan.SettlersOfCatan;
 import client.ClientGame;
+import enums.Colour;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
@@ -19,6 +21,7 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -26,12 +29,14 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.utils.Array;
 import grid.Hex;
-
+import grid.Node;
+import grid.Edge;
+import game.build.*;
 import java.awt.*;
 import java.util.Map.Entry;
+import java.util.List;
 
-public class GameScreen implements Screen
-{
+public class GameScreen implements Screen {
 	final private static Vector3 ORIGIN = new Vector3(0, 0, 0);
 	final private AssMan assets = new AssMan();
 	final private ModelBatch MODEL_BATCH = new ModelBatch();
@@ -53,8 +58,7 @@ public class GameScreen implements Screen
 	BitmapFont font;
 
 	// button creation
-	public void create()
-	{
+	public void create() {
 		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
 		font = new BitmapFont();
@@ -72,13 +76,11 @@ public class GameScreen implements Screen
 	}
 
 	// @Override
-	public void render()
-	{
+	public void render() {
 		stage.draw();
 	}
 
-	public GameScreen(final SettlersOfCatan game)
-	{
+	public GameScreen(final SettlersOfCatan game) {
 		this.game = game;
 		ClientGame gameState = game.getState();
 
@@ -96,8 +98,7 @@ public class GameScreen implements Screen
 		initEnvironment();
 	}
 
-	private void initCamera()
-	{
+	private void initCamera() {
 		cam = new PerspectiveCamera(50f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.position.set(0f, 8f, -10f);
 		cam.lookAt(0, 0, 0); // look at centre of world
@@ -106,8 +107,7 @@ public class GameScreen implements Screen
 		cam.update();
 	}
 
-	private void initBoard(ClientGame gameState)
-	{
+	private void initBoard(ClientGame gameState) {
 
 		final ModelBuilder builder = new ModelBuilder();
 		final long attributes = Usage.Position | Usage.Normal | Usage.TextureCoordinates;
@@ -129,15 +129,12 @@ public class GameScreen implements Screen
 		// hex tiles
 		final Model hex = builder.createCylinder(2.2f, 0.2f, 2.2f, 6, dirt, attributes);
 
-		System.out.println("****Entering init board.****");
-		for (Entry<Point, Hex> coord : gameState.getGrid().grid.entrySet())
-		{
+		for (Entry<Point, Hex> coord : gameState.getGrid().grid.entrySet()) {
 			final ModelInstance instance = new ModelInstance(hex, coord.getValue().get3DPos());
 			instance.transform.rotate(0, 1, 0, 90f);
 
 			final Color colour;
-			switch (coord.getValue().getResource())
-			{
+			switch (coord.getValue().getResource()) {
 			case Grain:
 				colour = Color.YELLOW;
 				break;
@@ -164,17 +161,125 @@ public class GameScreen implements Screen
 
 			instances.add(instance);
 		}
+
+		drawRoad();
+		drawBuilding();
 	}
 
-	private void initEnvironment()
-	{
+	private void initEnvironment() {
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1.0f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 	}
 
+	private void drawRoad() { 
+		Model model = assets.getModel("road.g3db"); 
+
+		List<Edge> edges = game.client.getState().getGrid().getEdgesAsList();
+		for (Edge edge : edges) { 
+			final Color colour;
+
+			Road road = edge.getRoad();
+
+			if (road!= null) {
+				Vector3 place = edge.get3dVectorMidpoint(edge);
+				ModelInstance instance = new ModelInstance(model, place);
+	
+				switch (Colour.BLUE) {
+				case BLUE:
+					colour = Color.BLUE;
+					break;
+				case RED:
+					colour = Color.RED;
+					break;
+				case WHITE:
+					colour = Color.WHITE;
+					break;
+				case ORANGE:
+					colour = Color.ORANGE;
+					break;
+	
+				default:
+					colour = null;
+					break;
+	
+				}
+	
+				instance.materials.get(0).set(ColorAttribute.createDiffuse(colour));
+				instance.transform.scale(0.1f, 0.1f, 0.1f);
+				Vector2 compare = edge.getX().get2DPos();
+				Vector2 compareTo = edge.getY().get2DPos();
+				
+				if (compare.x != compareTo.x)
+				{
+					if(compare.y > compareTo.y){
+						instance.transform.rotate(0,1,0,-60f);
+					} else {
+						instance.transform.rotate(0,1,0,60f);
+					}
+				}
+				
+				instances.add(instance);
+			}
+
+		}
+
+	}
+
+	public void drawBuilding() {
+		Model model = assets.getModel("settlement.g3db");
+		Model modelCity = assets.getModel("city.g3db");
+		
+		List<Node> nodes = game.client.getState().getGrid().getNodesAsList();
+		for (Node node : nodes) {
+			final Color colour;
+			Vector3 place = node.get3DPos();
+			ModelInstance instance;
+
+			Building building = node.getBuilding();
+			
+			 if (building == null) {
+				if (building instanceof Settlement) {
+				instance = new ModelInstance(model, place);
+				 } else {
+				 instance = new ModelInstance(modelCity, place);
+				 }
+	
+				switch (building.getPlayerColour()) {
+				case BLUE:
+					colour = Color.BLUE;
+					break;
+	
+				case RED:
+					colour = Color.RED;
+					break;
+	
+				case WHITE:
+					colour = Color.WHITE;
+					break;
+	
+				case ORANGE:
+					colour = Color.ORANGE;
+					break;
+	
+				default:
+					colour = null;
+					break;
+	
+				}
+	
+				instance.materials.get(0).set(ColorAttribute.createDiffuse(colour));
+				instance.transform.scale(0.2f, 0.2f, 0.2f);
+
+				instances.add(instance);
+
+			 }
+
+		}
+
+	}
+
 	@Override
-	public void render(final float delta)
-	{
+	public void render(final float delta) {
 		Gdx.gl.glClearColor(0 / 255, 128 / 255, 255 / 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
@@ -186,8 +291,7 @@ public class GameScreen implements Screen
 	}
 
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		assets.dispose();
 		MODEL_BATCH.dispose();
 		instances.clear();
@@ -196,27 +300,22 @@ public class GameScreen implements Screen
 	// Required but unused
 
 	@Override
-	public void resize(final int width, final int height)
-	{
+	public void resize(final int width, final int height) {
 	}
 
 	@Override
-	public void pause()
-	{
+	public void pause() {
 	}
 
 	@Override
-	public void resume()
-	{
+	public void resume() {
 	}
 
 	@Override
-	public void hide()
-	{
+	public void hide() {
 	}
 
 	@Override
-	public void show()
-	{
+	public void show() {
 	}
 }

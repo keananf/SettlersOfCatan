4756b2fc52a1bc3e55463a6efde7767929d00092
Liam McMenemie <lm251@st-andrews.ac.uk>
2017-04-17 19:34:50 +0100

    Update LocalgameSetupScreen to extend GameSetupScreen

--- core/src/main/java/catan/ui/LocalGameSetupScreen.java
+++ core/src/main/java/catan/ui/LocalGameSetupScreen.java
@@ -1,249 +1,49 @@
 package catan.ui;
+
 import AI.LocalAIClient;
 import catan.SettlersOfCatan;
 import client.Client;
 import client.LocalClient;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import enums.Difficulty;
 
-public class LocalGameSetupScreen implements Screen
+class LocalGameSetupScreen extends GameSetupScreen
 {
-	private final SettlersOfCatan game;
-	private boolean isAi;
-    private int numAIs;
-    private Difficulty difficulty = Difficulty.VERYEASY, opponentDifficulty = Difficulty.VERYEASY;
-    private String username = "Player";
-	private final Stage ui = new Stage(new ScreenViewport());
-
-	
-	LocalGameSetupScreen(final SettlersOfCatan game){
-		this.game = game;
-
-		Gdx.input.setInputProcessor(ui);
-		
-		Image background = new Image(new Texture(Gdx.files.internal("splash.jpg")));
-		background.setScaling(Scaling.fill);
-		background.setFillParent(true);
-		ui.addActor(background);
-
-		VerticalGroup body = new VerticalGroup();
-		body.setFillParent(true);
-		body.padTop(50);
-		body.space(60);
-		ui.addActor(body);
-		
-		TextField settings = new TextField("Settings",SettlersOfCatan.getSkin());
-		settings.setTextFieldListener((textField,c)-> textField.setText("Settings"));
-		body.addActor(settings);
-		
-		addOptions(body);
-		addSubmitButton(body);
-		addBackButton(body);
-		
-		
-	}
-	
-		private void addOptions(VerticalGroup body){
-			TextField Name = new TextField("NAME", SettlersOfCatan.getSkin());
-			Name.setTextFieldListener((textField,c)-> username = textField.getText());
-			body.addActor(Name);
-			
-			CheckBox checkBox = new CheckBox("Play as AI", SettlersOfCatan.getSkin());
-			checkBox.addListener(new InputListener(){
-				public void touchUp(InputEvent event, float x, float y, int pointer, int button){
-					super.touchUp(event, x, y, pointer, button);
-					checkBox.toggle();
-					isAi=checkBox.isChecked();
-				}   public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
-	            {
-	                super.touchUp(event, x, y, pointer, button);
-	                checkBox.toggle();
-	                isAi = checkBox.isChecked();
-	                return true;
-	            }
-	        });
-
-
-
-			// Number of AIs to play with. Remaining are remote players implicitly
-			TextField numAIsInput = new TextField("Number", SettlersOfCatan.getSkin());
-			numAIsInput.setTextFieldListener(new TextField.TextFieldListener()
-			{
-				@Override
-				public void keyTyped(TextField textField, char c)
-				{
-					textField.setText(String.valueOf(c));
-					int num = 0;
-					try
-					{
-						num = Integer.parseInt(textField.getText());
-					}
-					catch(NumberFormatException e)
-					{
-						textField.setText("Number");
-					}
-					numAIs = num;
-				}
-			});
-			
-	
-			
-		
-			
-			HorizontalGroup AISelection = new HorizontalGroup();
-			CheckBox aiSettings = new CheckBox("Easy(Random)",SettlersOfCatan.getSkin());
-			aiSettings.addListener(new InputListener(){
-				 public void touchUp(InputEvent event, float x, float y, int pointer, int button)
-		            {
-		                super.touchUp(event, x, y, pointer, button);
-						aiSettings.toggle();
-		                difficulty = Difficulty.VERYEASY;
-		            }
-
-		            @Override
-		            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
-		            {
-		                super.touchUp(event, x, y, pointer, button);
-						aiSettings.toggle();
-		                difficulty = Difficulty.VERYEASY;
-		                return true;
-		            }
-		        });
-		        CheckBox easySetting = new CheckBox("Medium", SettlersOfCatan.getSkin());
-		        easySetting.addListener(new InputListener()
-		        {
-		            @Override
-		            public void touchUp(InputEvent event, float x, float y, int pointer, int button)
-		            {
-		                super.touchUp(event, x, y, pointer, button);
-		                easySetting.toggle();
-		                difficulty = Difficulty.EASY;
-		            }
-
-		            @Override
-		            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
-		            {
-		                super.touchUp(event, x, y, pointer, button);
-		                easySetting.toggle();
-		                difficulty = Difficulty.EASY;
-		                return true;
-		            }
-		        });
-				
-			
-			
-		
-			AISelection.addActor(numAIsInput);
-			AISelection.addActor(aiSettings);
-			AISelection.addActor(easySetting);
-			body.addActor(checkBox);
-			body.addActor(AISelection);
-			
-		}
-		
-		
-		
-		
-		private void addBackButton(VerticalGroup body)
-		{
-			TextButton button = new TextButton("Main Menu", SettlersOfCatan.getSkin());
-			button.addListener(new ChangeListener()
-			{
-				public void changed(ChangeEvent event, Actor actor)
-				{
-					game.setScreen(new MainMenuScreen(game));
-				}
-			});
-
-			body.addActor(button);
-		}
-
-	private void addSubmitButton(VerticalGroup body)
+	LocalGameSetupScreen(final SettlersOfCatan game)
 	{
-		TextButton button = new TextButton("Submit", SettlersOfCatan.getSkin());
-		button.addListener(new ClickListener(){
-			public void clicked(InputEvent event, float x, float y){
-				super.clicked(event,x,y);
-				Client c;
-				if(isAi)
+		super("Start new game", game);
+
+		final NumberField numberOfAIs = new NumberField("Number of AI opponents");
+		addPrimary(numberOfAIs);
+
+		setSubmitListener(new ChangeListener()
+		{
+			@Override
+			public void changed(final ChangeEvent event, final Actor actor)
+			{
+				final Client client;
+				if (playerIsAI())
 				{
-					c= new LocalAIClient(difficulty, opponentDifficulty, game, username, numAIs);
-				} else{
-					c=new LocalClient(game,opponentDifficulty, username,numAIs);
+					client = new LocalAIClient(getChosenDifficulty(), game, getUsername(), numberOfAIs.getNumericValue());
 				}
-				game.startNewServer(c);
+				else
+				{
+					client = new LocalClient(game, getUsername(), numberOfAIs.getNumericValue());
+				}
+				game.startNewServer(client);
 				game.setScreen(new GameScreen(game));
 			}
 		});
-		body.addActor(button);
-
 	}
-	@Override
-	public void show()
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void render(float delta)
-	{
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		ui.act(delta);
-		ui.draw();
-
-	}
-
-	@Override
-	public void resize(int width, int height)
-	{
-		ui.getViewport().update(width, height, true);
-
-	}
-
-	@Override
-	public void pause()
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void resume()
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void hide()
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose()
-	{
-		ui.dispose();
-
-	}
-
 }
-	
-	
-	
-	
-
